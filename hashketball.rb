88378abd98b7af91833440de9afc280515e36971
Keith Williams <thestreetkw@hotmail.com>
2020-10-16 05:50:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,23 +20,23 @@ def game_hash
           :player_name => "Reggie Evans",
           :number => "30",
           :shoe => "14",
-          :points => 12,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 12,
-          :blocks => 12,
-          :slam_dunks => 7
+          :points => "12",
+          :rebounds => "12",
+          :assists => "12",
+          :steals => "12",
+          :blocks => "12",
+          :slam_dunks => "7"
         },
         :brook_lopez => {
           :player_name => "Brook Lopez",
-          :number => 11,
-          :shoe => 17,
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15
+          :number => "11",
+          :shoe => "17",
+          :points => "17",
+          :rebounds => "19",
+          :assists => "10",
+          :steals => "3",
+          :blocks => "1",
+          :slam_dunks => "15"
         },
         :mason_plumlee => {
           :player_name => "Mason Plumlee",
